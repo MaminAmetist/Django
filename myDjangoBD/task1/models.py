eaf3685393863820e1650b332373f95cf4b82d2a
Анна Нездулькина <anna.nezdulkina@mail.ru>
2024-12-16 22:06:43 +0700

    Update models.py

--- myDjangoBD/task1/models.py
+++ myDjangoBD/task1/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Buyer(models.Model):
     name = models.CharField(max_length=20)
     balance = models.DecimalField(max_digits=5, decimal_places=2)
-    age = models.IntegerField()
+    age = models.PositiveSmallIntegerField()
 
     def __str__(self):
         return self.name
